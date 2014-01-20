8e55410784b16202bd08384213afbaf29ca3e3dd
danb121 <daniel.borg@orange.net>
2014-01-20 14:36:08 +0000

    added some comments

--- lcd_menu/lcd_menu.ino
+++ lcd_menu/lcd_menu.ino
@@ -1,5 +1,8 @@
+
+//Import LCD library
 #include <LiquidCrystal.h>
 
+//These values won't change
 #define BUTTON_DOWN_RIGHT A2
 #define BUTTON_UP_LEFT A3
 
@@ -10,59 +13,20 @@
 #define DOWN 8
 #define ALL 16
 
-int buttonPressed = 0;
-int lastButton = 0;
-int currentmenu = 1;
-int submenu = 0;
-int x = 0;
-int y = 0;
-int button;
+int buttonPressed = 0;  //Have any buttons been pressed, used for leaving the front screen
+int lastButton = 0; //Not used at the moment
+int currentMenu = 1; //Keep track on the current menu number
+int intervalometerMenu = 0; //Has this menu been accessed
+int lightMenu = 0;  //Has this menu been accessed
+int soundMenu = 0;  //Has this menu been accessed
+int x = 0;  //Cursor position
+int y = 0;  //Cursor position
+int button;  //Not used at the moment
 
+//LCD I/O Pins
 LiquidCrystal lcd(12,11,8,7,6,5);
 
-byte down[8] = {
-  B00000,
-  B00100,
-  B00100,
-  B10101,
-  B01110,
-  B00100,
-  B00000
-};
-
-byte up[8] = {
-  B00000,
-  B00100,
-  B01110,
-  B10101,
-  B00100,
-  B00100,
-  B00000
-};
-#include <LiquidCrystal.h>
-
-#define BUTTON_DOWN_RIGHT A2
-#define BUTTON_UP_LEFT A3
-
-#define NONE 0
-#define LEFT 1
-#define RIGHT 2
-#define UP 4
-#define DOWN 8
-#define ALL 16
-
-int buttonPressed = 0;
-int lastButton = 0;
-int currentMenu = 1;
-int intervalometerMenu = 0;
-int lightMenu = 0;
-int soundMenu = 0;
-int x = 0;
-int y = 0;
-int button;
-
-LiquidCrystal lcd(12,11,8,7,6,5);
-
+//Custom Characters
 byte down[8] = {
   B00000,
   B00100,
@@ -103,6 +67,7 @@ byte left[8] ={
   B00000
 };
 
+
 void setup()
 {
   pinMode(BUTTON_UP_LEFT, INPUT);
@@ -116,6 +81,7 @@ void setup()
   reset();
 }
 
+//Function used to rest certain parameters
 void reset()
 {
   lcd.clear();
@@ -137,23 +103,27 @@ void reset()
 
 void loop()
 {
+  //Check for button input
   button = checkButton();
+  //Go to Frontscreen
   frontscreen ();
   delay(250);
 /*
-  //cursorPos(button, x, y);
+  //menuposmenupos(button, x, y);
   lcd.cursor();
   lcd.setCursor(x,y);
   delay(100);
 */
 }
 
-
+//Check for button input
 byte checkButton()
 {
+  //Assign variable for reading input
   int a1 = analogRead(BUTTON_UP_LEFT);
   int a2 = analogRead(BUTTON_DOWN_RIGHT);
 
+  //Check Input and return value based on buttons pressed
   if (a1 < 700 && a2 < 700){
     reset();
   }
@@ -169,7 +139,8 @@ byte checkButton()
     return RIGHT;
 }
 
-void cursorpos(int state, int& pos)
+//Work out menu position based on button pressed
+void menupos(int state, int& pos)
 {
   if (state == 2) {
     pos = pos * 10;
@@ -182,8 +153,10 @@ void cursorpos(int state, int& pos)
     }
 }
 
+//Front screen
 void frontscreen()
 {
+  //series of if statements to check for submenu access and if buttons have been pressed
   if (intervalometerMenu == 1){
     intervalometer();
   } else if (lightMenu == 1){
@@ -210,16 +183,20 @@ void frontscreen()
   }
 }
 
+//Display menu items
 void displaymenu()
 {
-  cursorpos(button, currentMenu);
+  //use mempos function to update the current menu based on button pressed
+  menupos(button, currentMenu);
   
+  //If statement to make sure that the currentmenu doesn't got out of scope
   if (currentMenu < 1){
     currentMenu = 1;
   } else if (currentMenu > 3 && currentMenu < 10){
     currentMenu = 3;
   }
   Serial.println(currentMenu);
+  //switchcase statements to display text and set variables based on currentmenu
   switch(currentMenu) {
   case 1:
     lcd.clear();
@@ -248,16 +225,20 @@ void displaymenu()
   }
 }
 
+//intervalometer submenu
 void intervalometer()
 {
-  cursorpos(button, currentMenu);
+  //use mempos function to update the current menu based on button pressed
+  menupos(button, currentMenu);
   
+  //If statement to make sure that the currentmenu doesn't got out of scope
   if (currentMenu <= 10){
     currentMenu = 10;
   } else if (currentMenu >= 16 && currentMenu < 20){
     currentMenu = 16;
   }
   Serial.println(currentMenu);
+  //switchcase statements to display text and set variables based on currentmenu
   switch(currentMenu) {
   case 10:
     lcd.clear();
@@ -321,6 +302,7 @@ void intervalometer()
   }
 }
 
+//Test function... future use
 void light()
 {
   lcd.clear();
@@ -328,154 +310,10 @@ void light()
   lcd.print("Light Sensor");
 }
 
+//Test function... future use
 void sound()
 {
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("Sound Sensor");
-}
-byte right[8] ={
-  B00000,
-  B00100,
-  B00010,
-  B11111,
-  B00010,
-  B00100,
-  B00000
-};
-
-byte left[8] ={
-  B00000,
-  B00100,
-  B01000,
-  B11111,
-  B01000,
-  B00100,
-  B00000
-};
-
-void setup()
-{
-  pinMode(BUTTON_UP_LEFT, INPUT);
-  pinMode(BUTTON_DOWN_RIGHT, INPUT);
-  lcd.begin(16,2);
-  Serial.begin(9600);
-  lcd.createChar(1,down);
-  lcd.createChar(2,up);
-  lcd.createChar(3,right);
-  lcd.createChar(4,left);
-  reset();
-}
-
-void reset()
-{
-  lcd.clear();
-  delay(500);
-  lcd.setCursor(0,0);
-  lcd.print("Welcome");
-  delay(3000);
-  lcd.clear();
-  buttonPressed = 0;
-  lastButton = 0;
-  currentmenu = 1;
-  x = 0;
-  y = 0;
-}
-
-void loop()
-{
-  button = checkButton();
-  frontscreen ();
-  delay(250);
-/*
-  //cursorPos(button, x, y);
-  lcd.cursor();
-  lcd.setCursor(x,y);
-  delay(100);
-*/
-}
-
-
-byte checkButton()
-{
-  int a1 = analogRead(BUTTON_UP_LEFT);
-  int a2 = analogRead(BUTTON_DOWN_RIGHT);
-
-  if (a1 < 700 && a2 < 700){
-    reset();
-  }
-  if (a1 > 500 && a2 > 500)
-    return NONE;
-  if (a1 < 10)
-    return LEFT;
-  if (a1 > 10 && a1 < 500)
-    return UP;
-  if (a2 < 10)
-    return DOWN;
-  if (a2 > 10 && a2 < 500)
-    return RIGHT;
-}
-
-void cursorpos(int state, int& pos)
-{
-  if (state == 2) {
-    pos = pos * 10;
-    }
-  if (state == 4) {
-    pos = pos - 1;
-    }
-  if (state == 8) {
-    pos = pos + 1;
-    }
-}
-
-void frontscreen()
-{
-  if (buttonPressed == 1){
-    displaymenu();
-  } else if (button == 1 || button == 2 || button == 4 || button == 8){
-    buttonPressed = 1;
-  } else {
-    lcd.setCursor(0,0);
-    lcd.print("Use Arrows To");
-    lcd.setCursor(0,1);
-    lcd.print("Select Menu");
-    lcd.setCursor(12,1);
-    lcd.write(1);
-    lcd.setCursor(13,1);
-    lcd.write(2);
-    lcd.setCursor(14,1);
-    lcd.write(3);
-    lcd.setCursor(15,1);
-    lcd.write(4);
-  }
-}
-
-void displaymenu()
-{
-  cursorpos(button, currentmenu);
-  
-  if (currentmenu < 1){
-    currentmenu = 0;
-  } else if (currentmenu > 3 && currentmenu < 10){
-    currentmenu = 3;
-  }
-  
-  switch(currentmenu) {
-  case 1:
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Intervalomter");
-    break;
-  case 2:
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Light Trigger");
-    break;
-  case 3:
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Sound Trigger");
-    break;
-  }
 }
