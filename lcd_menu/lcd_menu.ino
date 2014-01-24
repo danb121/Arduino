1b752d92252a718b55933db31de2f9aff340cbf4
danb121 <daniel.borg@orange.net>
2014-01-24 15:34:54 +0000

    added arrays for storing time and shot info
    added 3 arrays for adjusting and storing time and shot info, also added the ability to adjust these values using the keypad!

--- lcd_menu/lcd_menu.ino
+++ lcd_menu/lcd_menu.ino
@@ -10,6 +10,8 @@
 #include <LiquidCrystal.h>
 
 //These values won't change
+
+#define DEBUG
 #define BUTTON_MENU A2
 #define BUTTON_IP A3
 
@@ -21,7 +23,7 @@
 #define ENTER 16
 #define M_UP 32
 #define M_DOWN 64
-//#define ALL 128
+
 
 int menuEntered = 0;  //Have any buttons been pressed, used for leaving the front screen
 int currentMenu = 1; //Keep track on the current menu number
@@ -34,6 +36,15 @@ int lastButton = 0; //Not used at the moment
 int cursorMoved = 0; //Used to hold if the cursor has been adjusted state
 int x = 0;  // Left to Right Cursor position
 int y = 0;  //Up and Down Cursor position
+char shutterTime[6];  //Array for storing Shutter time
+char delayTime[6];  //Array for storing shot delay
+char numShots[4]; //Array for storing the number of photos.
+int shut = 0;  //used to keep track of shutterTime array position
+int del = 0;  //used to keep track of delayTime array position
+int shot = 0; //used to keep track of delayTime array position
+int shutVal = 0;  //used for storing and checking that the shutter time value hasn't gone out of scope
+int delVal = 0;  //used for storing and checking that the delay time value hasn't gone out of scope
+int shotVal = 0;  //used for storing and checking that the shot value hasn't gone out of scope
 
 //LCD I/O Pins
 LiquidCrystal lcd(12,11,8,7,6,5);
@@ -85,7 +96,11 @@ void setup()
   pinMode(BUTTON_MENU, INPUT);
   pinMode(BUTTON_IP, INPUT);
   lcd.begin(16,2);
+  
+  #ifdef DEBUG
   Serial.begin(9600);
+  #endif
+  
   lcd.createChar(1,down);
   lcd.createChar(2,up);
   lcd.createChar(3,right);
@@ -110,8 +125,23 @@ void reset()
   intervalometerMenu = 0;
   lightMenu = 0;
   soundMenu = 0;
+  shut = 0; 
+  del = 0;
+  shot = 0;
+  shutVal = 0;
+  delVal = 0;
+  shotVal = 0;
   x = 0;
   y = 0;
+  for (int i=0; i < 6; i++){
+    shutterTime[i] = 0;
+  }
+  for (int i=0; i < 6; i++){
+    delayTime[i] = 0;
+  }
+  for (int i=0; i < 4; i++){
+    numShots[i] = 0;
+  }
 }
 
 void loop()
@@ -131,10 +161,12 @@ byte checkButton()
   int a1 = analogRead(BUTTON_IP);
   int a2 = analogRead(BUTTON_MENU);
   
-//  Serial.print ("a1 = ");
-//  Serial.println (a1);
-//  Serial.print ("a2 = ");
-//  Serial.println (a2);
+  #ifdef DEBUG
+  Serial.print ("a1 = ");
+  Serial.println (a1);
+  Serial.print ("a2 = ");
+  Serial.println (a2);
+  #endif
   
   //Check Input and return value based on buttons pressed
   if (a1 > 245 && a1 < 275){
@@ -172,30 +204,18 @@ void menupos(int state, int& pos)
     }
 }
 
-void curpos(int state, int& posx, int& posy)
+void time(int state, char& num)
 {
-  if (state == 1) {
-    posx = posx - 1;
-    if (posx <= 0){
-      posx = 0;
-    }
-  }
-  if (state == 2) {
-    posx = posx + 1;
-    if (posx >= 16){
-      posx = 16;
-    }
-  }
   if (state == 4) {
-    posy = posy - 1;
-    if (posy <= 0){
-      posy = 0;
-    }
+    num = num +1;
+    if (num >= 9){
+      num = 9;
+    }  
   }
   if (state == 8) {
-    posy = posy + 1;
-    if (posy >= 1){
-      posy = 1;
+    num = num -1;
+    if (num <= 0){
+      num = 0;
     }
   }
 }
@@ -219,14 +239,6 @@ void frontscreen()
     lcd.print("Press Enter to");
     lcd.setCursor(0,1);
     lcd.print("Continue");
-    //lcd.setCursor(12,1);
-    //lcd.write(1);
-    //lcd.setCursor(13,1);
-    //lcd.write(2);
-    //lcd.setCursor(14,1);
-    //lcd.write(3);
-    //lcd.setCursor(15,1);
-    //lcd.write(4);
   }
 }
 
@@ -242,45 +254,48 @@ void displaymenu()
   } else if (currentMenu > 3 && currentMenu < 10){
     currentMenu = 3;
   }
+  
+    #ifdef DEBUG
     Serial.print ("button = ");
     Serial.println (button);  
     Serial.print ("Current Menu = ");
     Serial.println(currentMenu);
     Serial.print ("Last Menu = ");
     Serial.println(lastMenu);
+    #endif
 
   //Check to see if the menu has changed
   if (currentMenu != lastMenu){
   
     //switchcase statements to display text and set variables based on currentmenu
     switch(currentMenu) {
-    case 1:
-      lastMenu = currentMenu;
-      lcd.clear();
-      lcd.setCursor(0,0);
-      lcd.print("Intervalomter");
-      break;
-    case 2:
-      lastMenu = currentMenu;
-      lcd.clear();
-      lcd.setCursor(0,0);
-      lcd.print("Light Trigger");
-      break;
-    case 3:
-      lastMenu = currentMenu;
-      lcd.clear();
-      lcd.setCursor(0,0);
-      lcd.print("Sound Trigger");
-      break;
-    case 10:
-      intervalometerMenu = 1;
-      break;
-    case 20:
-      lightMenu = 1;
-      break;
-    case 30:
-      soundMenu = 1;
-      break;
+      case 1:
+        lastMenu = currentMenu;
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Intervalomter");
+        break;
+      case 2:
+        lastMenu = currentMenu;
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Light Trigger");
+        break;
+      case 3:
+        lastMenu = currentMenu;
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Sound Trigger");
+        break;
+      case 10:
+        intervalometerMenu = 1;
+        break;
+      case 20:
+        lightMenu = 1;
+        break;
+      case 30:
+        soundMenu = 1;
+        break;
     }
   }
 }
@@ -291,18 +306,24 @@ void intervalometer()
   //use mempos function to update the current menu based on button pressed
   menupos(button, currentMenu);
   
-    //If statement to make sure that the currentmenu doesn't got out of scope
+  //Create buffer for printing values to LCD
+  char lcd_buffer[11];
+  
+  //If statement to make sure that the currentmenu doesn't got out of scope
   if (currentMenu <= 10 || currentMenu == 100){
     currentMenu = 10;
   } else if (currentMenu >= 13 && currentMenu < 20){
     currentMenu = 13;
   }
   
-//    Serial.print ("Current Menu = ");
-//    Serial.println(currentMenu);
-//    Serial.print ("Last Menu = ");
-//    Serial.println(lastMenu);
-
+    #ifdef DEBUG
+    Serial.print ("Current Menu = ");
+    Serial.println(currentMenu);
+    Serial.print ("Last Menu = ");
+    Serial.println(lastMenu);
+    #endif
+      
+  //quick test to see if directional buttons have been pressed
   if (button == 1 || button == 2 || button == 4 || button == 8){
     cursorMoved = 1;
   }
@@ -311,59 +332,212 @@ void intervalometer()
     
     //switchcase statements to display text and set variables based on currentmenu
     switch(currentMenu) {
-    case 10:
-      lastMenu = currentMenu;
-      lcd.clear();
-      lcd.setCursor(0,0);
-      lcd.print("Shutter Duration");
-      lcd.setCursor(0,1);
-      lcd.print("00h 00m 00s");
-      Serial.println (button);
-      if (cursorMoved == 1){
-        
-        //use curpos function to update the current cursor position based on button pressed
-        curpos(button, x, y);
+      case 10:
+        if (currentMenu != lastMenu){
+          x = 0;
+          lcd.clear();
+          lcd.setCursor(0,0);
+          lcd.print("Shutter Duration");
+        }
+          lcd.setCursor(0,1);
+          //Load data into buffer
+          sprintf(lcd_buffer,"%d%dh %d%dm %d%ds", shutterTime[0],shutterTime[1],shutterTime[2],shutterTime[3],shutterTime[4],shutterTime[5]);
+          lcd.print(lcd_buffer);
+          lcd.setCursor(x,1);
+          lcd.cursor();
       
-        lcd.setCursor(x,x);
+        if (cursorMoved == 1){
+          switch (button) {
+            case RIGHT:
+              //increment cursor position by one
+              x++;
+              
+              //skip spaces where data entry isn't required and make sure that position doesn't go out of scope
+              if (x == 2 || x == 4 || x == 6){
+                x = x+2;
+              } else if (x >= 9){
+                x = 9;
+              }
+              //increment array position by one and make sure that it doesn't go out of scope
+              shut++;
+              if (shut >= 5){
+                shut = 5;
+              }
+              lcd.setCursor(x,1);
+              lcd.cursor();
+              break;
+            
+            case LEFT:
+              //decrement cursor position by one
+              x--;
+              
+              //skip spaces where data entry isn't required and make sure that position doesn't go out of scope
+              if (x == 3 || x == 7){
+                x = x-2;
+              } else if (x <= 0){
+                x = 0;
+              }
+              //deccrement array position by one and make sure that it doesn't go out of scope
+              shut--;
+              if (shut <= 0){
+                shut = 0;
+              }
+              lcd.setCursor(x,1);
+              lcd.cursor();
+              break;
+
+            case UP:
+              time(button,shutterTime[shut]);
+              break;
+            
+            case DOWN:
+              time(button,shutterTime[shut]);
+              break;
+          }
+          lastMenu = currentMenu;
+          //lastButton = button;
+        }
+        break;
+      case 11:
+        if (currentMenu != lastMenu){
+          x = 0;
+          lcd.clear();
+          lcd.setCursor(0,0);
+          lcd.print("Shot Delay");
+         }
+          lcd.setCursor(0,1);
+          sprintf(lcd_buffer,"%d%dh %d%dm %d%ds", delayTime[0],delayTime[1],delayTime[2],delayTime[3],delayTime[4],delayTime[5]);
+          lcd.print(lcd_buffer);
+          lcd.setCursor(x,1);
+          lcd.cursor();
+      
+        if (cursorMoved == 1){
+          switch (button) {
+            case RIGHT:
+            //increment cursor position by one
+              x++;
+              
+              //skip spaces where data entry isn't required and make sure that position doesn't go out of scope
+              if (x == 2 || x == 4 || x == 6){
+                x = x+2;
+              } else if (x >= 9){
+                x = 9;
+              }
+              //increment array position by one and make sure that it doesn't go out of scope
+              del++;
+              if (del >= 5){
+                del = 5;
+              }
+              lcd.setCursor(x,1);
+              lcd.cursor();
+              break;
+            
+            case LEFT:
+              //decrement cursor position by one
+              x--;
+              
+              //skip spaces where data entry isn't required and make sure that position doesn't go out of scope
+              if (x == 3 || x == 7){
+                x = x-2;
+              } else if (x <= 0){
+                x = 0;
+              }
+              //deccrement array position by one and make sure that it doesn't go out of scope
+              del--;
+              if (del <= 0){
+                del = 0;
+              }
+              lcd.setCursor(x,1);
+              lcd.cursor();
+              break;
+
+            case UP:
+              time(button,delayTime[del]);
+              break;
+            
+            case DOWN:
+              time(button,delayTime[del]);
+              break;
+          }
+          lastMenu = currentMenu;
+        }
+        break;
+      case 12:
+        if (currentMenu != lastMenu){
+          x = 0;
+          lcd.clear();
+          lcd.setCursor(0,0);
+          lcd.print("Number of Shots");
+         }
+          lcd.setCursor(0,1);
+          sprintf(lcd_buffer,"%d%d%d%d", numShots[0],numShots[1],numShots[2],numShots[3]);
+          lcd.print(lcd_buffer);
+          lcd.setCursor(x,1);
+          lcd.cursor();
+      
+        if (cursorMoved == 1){
+          switch (button) {
+            case RIGHT:
+            //increment cursor position by one
+              x++;
+              
+              //skip spaces where data entry isn't required and make sure that position doesn't go out of scope
+              if (x >= 3){
+                x = 3;
+              }
+              //increment array position by one and make sure that it doesn't go out of scope
+              shot++;
+              if (shot >= 3){
+                shot = 3;
+              }
+              lcd.setCursor(x,1);
+              lcd.cursor();
+              break;
+            
+            case LEFT:
+              //decrement cursor position by one
+              x--;
+              
+              //skip spaces where data entry isn't required and make sure that position doesn't go out of scope
+              if (x <= 0){
+                x = 0;
+              }
+              //deccrement array position by one and make sure that it doesn't go out of scope
+              shot--;
+              if (shot <= 0){
+                shot = 0;
+              }
+              lcd.setCursor(x,1);
+              lcd.cursor();
+              break;
+
+            case UP:
+              time(button,numShots[shot]);
+              break;
+            
+            case DOWN:
+              time(button,numShots[shot]);
+              break;
+          }
+          lastMenu = currentMenu;
+        }
+        break;
+      case 13:
+        lastMenu = currentMenu;
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Back");
+        lcd.setCursor(0,0);
         lcd.cursor();
-      }
-      break;
-    case 11:
-      lastMenu = currentMenu;
-      lcd.clear();
-      lcd.setCursor(0,0);
-      lcd.print("Shot Delay");
-      lcd.setCursor(0,1);
-      lcd.print("00h 00m 00s");
-      lcd.setCursor(0,1);
-      lcd.cursor();
-      break;
-    case 12:
-      lastMenu = currentMenu;
-      lcd.clear();
-      lcd.setCursor(0,0);
-      lcd.print("Num Shots");
-      lcd.setCursor(0,1);
-      lcd.print("00000");
-      lcd.setCursor(0,1);
-      lcd.cursor();
-      break;
-    case 13:
-      lastMenu = currentMenu;
-      lcd.clear();
-      lcd.setCursor(0,0);
-      lcd.print("Back");
-      lcd.setCursor(0,0);
-      lcd.cursor();
-      break;
-    case 130:
-      lcd.noCursor();
-      currentMenu = 1;
-      button = NONE;
-      menuEntered = 1;
-      intervalometerMenu = 0;
-      frontscreen();
-      break;
+        break;
+      case 130:
+        lcd.noCursor();
+        currentMenu = 1;
+        button = NONE;
+        menuEntered = 1;
+        intervalometerMenu = 0;
+        frontscreen();
+        break;
     }
   }
 }
